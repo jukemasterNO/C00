ca59ff3c073dddd6539f5db1d1fa76fa53156925
jukemasterNO <pawaris.sabuathong@gmail.com>
2026-07-13 16:49:26 +0700

    Update ft_print_numbers.c

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -1,11 +1,18 @@
 #include <unistd.h>
 
-void ft_print_numbers(void)
+void    ft_print_numbers(void)
 {
-    int digit = '0';
-    while(digit <= '9')
-    {
-        write(1, &digit, 1);
-        digit++;
-    }
+        int             digit;
+
+        digit = '0';
+        while (digit <= '9' )
+        {
+                write (1, &digit, 1);
+                digit++;
+        }
+}
+
+int     main(void)
+{
+        ft_print_numbers();
 }
