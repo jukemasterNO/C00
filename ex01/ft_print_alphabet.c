dba21291b8f595d5a7bcda8cab1f93378205dab2
jukemasterNO <pawaris.sabuathong@gmail.com>
2026-07-13 16:40:10 +0700

    Update ft_print_alphabet.c

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -1,11 +1,18 @@
 #include <unistd.h>
 
-void ft_print_alphabet(void)
+void    ft_print_alphabet(void)
 {
-    char alphabet = 'a';
-    while (alphabet <= 'z')
-    {
-        write (1, &alphabet, 1);
-        alphabet++;
-    }
+        char    alphabet;
+
+        alphabet = 'a';
+        while (alphabet <= 'z')
+        {
+                write(1, &alphabet, 1);
+                alphabet++;
+        }
+}
+
+int     main(void)
+{
+        ft_print_alphabet();
 }
