da800e7e45885f2839e5ae8431d76a4a5948f9cf
jukemasterNO <pawaris.sabuathong@gmail.com>
2026-07-13 17:29:24 +0700

    Update ft_is_negative.c

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -1,15 +1,21 @@
 #include <unistd.h>
 
-void ft_is_negative(int n)
+void    ft_is_negative(int n)
 {
-    char status;
-    if (n >= 0)
-    {
-        status = 'P';    
-    }
-    else 
-    {
-        status = 'N';
-    }
-    write(1, &status, 1);
+        char    status;
+
+        if (n >= 0)
+        {
+                status = 'P';
+        }
+        else
+        {
+                status = 'N';
+        }
+        write (1, &status, 1);
+}
+
+int     main(void)
+{
+        ft_is_negative(1);
 }
