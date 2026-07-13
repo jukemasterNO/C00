6dc9e3079152a4e5def7f2fc07ebda541b18a21d
jukemasterNO <pawaris.sabuathong@gmail.com>
2026-07-13 16:41:35 +0700

    Update ft_print_reverse_alphabet.c

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,11 +1,18 @@
 #include <unistd.h>
 
-void ft_print_reverse_alphabet(void)
+void    ft_print_reverse_alphabet(void)
 {
-    char alphabet = 'z';
-    while (alphabet >= 'a')
-    {
-        write (1, &alphabet, 1);
-        alphabet--;
-    }
+        char    alphabet;
+
+        alphabet = 'z';
+        while (alphabet >= 'a')
+        {
+                write (1, &alphabet, 1);
+                alphabet--;
+        }
+}
+
+int     main(void)
+{
+        ft_print_reverse_alphabet();
 }
